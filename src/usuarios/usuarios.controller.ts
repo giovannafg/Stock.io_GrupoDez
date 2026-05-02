af1ac83d8da8fce76c79e0353412d2c4f7bed8fc
Zayra-Moraes <zayramoraes1212@gmail.com>
2026-05-02 10:25:26 -0300

    adicionando rotas

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 
 @Controller('usuarios')
@@ -9,4 +9,25 @@ export class UsuariosController {
     getAllUsers(){
         return this.usuariosService.List_all()
     }
+
+    @Post()
+    creat(){
+        return "adicionar no banco um"
+    }
+
+    @Get("/:id")
+    getById(@Param('id')id:string){
+        return "adicionando pelo id"
+    }
+
+    @Patch("/:id")
+    attById(@Param('id')id:string){
+        return "att user"
+    }
+
+    @Delete("/:id")
+    deleteById(@Param('id')id:string){
+        return "Deletando user"
+    }
+
 }
