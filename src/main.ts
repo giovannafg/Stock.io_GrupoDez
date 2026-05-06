45671b4a8ba16a6de050326f64e741cc88dbfe34
giovannafg <giovannafelp@gmail.com>
2026-05-06 17:48:43 -0300

    feat(validation): implementar class-validator nos DTOs e ativar ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import 'dotenv/config';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app/app.module';
 
 //Mudei a porta para 3001 para usar a 3000 pro front
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
